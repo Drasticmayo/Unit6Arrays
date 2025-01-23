060e9f911b9b7ff4fcef2cb8e6f04040b7403c16
Jude Armstrong <134647784+Drasticmayo@users.noreply.github.com>
2025-01-23 09:35:27 -0500

    Update FirstRunner.java

--- FirstRunner.java
+++ FirstRunner.java
@@ -1,11 +1,13 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Jude Armstrong
 //Date -
 
 public class FirstRunner
 {
 	public static void main( String args[] )
 	{
+        int[] ray = {9,8,7,6,5,4,3,2,0,-2,9,9};
+        System.out.println(RayFirst.go(ray));
 	}
 }
