9ab8c07bf66d3a2261e912b9f2baf1eb5baa075a
Jude Armstrong <134647784+Drasticmayo@users.noreply.github.com>
2025-01-23 09:36:59 -0500

    Update SmallestRunner.java

--- SmallestRunner.java
+++ SmallestRunner.java
@@ -1,11 +1,13 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Jude Armstrong
 //Date -
 
 public class SmallestRunner
 {
 	public static void main( String args[] )
-	{									
+	{	
+        int[] ray = {-99,1,2,3,4,5,6,7,8,9,10,12345};
+		System.out.println(RaySmallest.go(ray));
 	}
 }
