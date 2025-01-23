51a8a3f1e799a3e2256e789720252da6a6464071
Jude Armstrong <134647784+Drasticmayo@users.noreply.github.com>
2025-01-23 09:35:56 -0500

    Update RayFirst.java

--- RayFirst.java
+++ RayFirst.java
@@ -1,12 +1,24 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Jude Armstrong
 //Date -
 
 public class RayFirst
 {
 	public static boolean go(int[] ray)
 	{
-		return true;
+		if (ray.length == 0)
+        {
+            return false;
+        }
+
+        for (int k = 1; k < ray.length; k++)
+        {
+            if (ray[k] == ray[0])
+            {
+                return true;
+            }
+        }
+        return false;
 	}
 }
